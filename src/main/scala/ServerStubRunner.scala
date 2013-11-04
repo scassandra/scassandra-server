86a25f8e04d56d9d785b903e3070eae7123c160a
Christopher Batey <christopher.batey@gmail.com>
2013-11-04 17:21:34 +0000

    Server only sends a READY message if a STARTUP message header is received. No validation on the body of the STARTUP message is done.

--- src/main/scala/ServerStubRunner.scala
+++ src/main/scala/ServerStubRunner.scala
@@ -1,16 +1,19 @@
+import java.io.{DataInputStream, DataOutputStream, OutputStreamWriter, PrintWriter}
 import com.typesafe.scalalogging.slf4j.Logging
-import java.io.DataOutputStream
 import java.net.{ServerSocket, Socket}
+
+object OpCodes {
+  val Ready : Byte = 0x02
+  val Startup : Byte = 0x01
+  val Options : Byte = 0x05
+}
 
 object ResponseHeader {
   val VersionByte = 0x82
-  val FlagsNoCompressionByte : Byte = 0x00
-  val DefaultStreamId : Byte = 0x00
-  val ZeroLength : Array[Byte] = Array[Byte](0x00, 0x00, 0x00, 0x00)
+  val FlagsNoCompressionByte = 0x00
+  val DefaultStreamId = 0x00
+  val ZeroLength = Array(0x00, 0x00, 0x00, 0x00).map(_.toByte)
 
-  object OpCodes {
-    val Ready = 0x02
-  }
 
 }
 
@@ -35,17 +38,26 @@ object ServerStubRunner extends Logging {
 
       // TODO: "java.net.SocketException: Connection reset" could happen if the socket is closed from the other side while this is still trying to write to it.
       val out = new DataOutputStream(clientSocket.getOutputStream)
+      val in = new DataInputStream(clientSocket.getInputStream)
 
-      logger.info("Sending startup message")
-      out.write(ResponseHeader.VersionByte)
-      out.write(ResponseHeader.FlagsNoCompressionByte)
-      out.write(ResponseHeader.DefaultStreamId)
-      out.write(ResponseHeader.OpCodes.Ready)
-      out.write(ResponseHeader.ZeroLength)
-      out.flush()
+      // Read header + length
+      val header : Seq[Int] = for {
+        i <- 0 until 4
+      } yield in.read()
+
+      header.foreach(print)
+
+      if (header(3) == OpCodes.Ready) {
+        logger.info("Sending startup message")
+        out.write(ResponseHeader.VersionByte)
+        out.write(ResponseHeader.FlagsNoCompressionByte)
+        out.write(ResponseHeader.DefaultStreamId)
+        out.write(OpCodes.Ready)
+        out.write(ResponseHeader.ZeroLength)
+        out.flush()
+      }
 
       // TODO: call close on the socket, or the output stream, or both?
-      logger.info("Closing socket")
       clientSocket.close()
     }
   }
