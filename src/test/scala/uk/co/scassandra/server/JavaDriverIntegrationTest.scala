1b5648f4eeff8035710a0ca7821d4e584127a617
Christopher Batey <christopher.batey@gmail.com>
2014-03-10 17:46:27 -0600

    Adding integration test for a row with multiple columns

--- src/test/scala/uk/co/scassandra/server/JavaDriverIntegrationTest.scala
+++ src/test/scala/uk/co/scassandra/server/JavaDriverIntegrationTest.scala
@@ -53,4 +53,25 @@ class JavaDriverIntegrationTest extends AbstractIntegrationTest with ScalaFuture
     cluster.close()
   }
 
+  test("Test prime and query with many columns") {
+    // priming
+    val whenQuery = "select * from people"
+    val svc = url("http://localhost:8043/prime") << s""" {"when":"${whenQuery}", "then": [{"name":"Chris", "age":"28"}, {"name":"Alexandra", "age":"24"}] } """  <:< Map("Content-Type" -> "application/json")
+    val response = Http(svc OK as.String)
+    response()
+
+    val cluster = Cluster.builder().addContactPoint("localhost").withPort(8042).build()
+    val session = cluster.connect("people")
+    val result = session.execute("select * from people")
+
+    val results = result.all()
+    results.size() should equal(2)
+    results.get(0).getString("name") should equal("Chris")
+    results.get(0).getString("age") should equal("28")
+    results.get(1).getString("name") should equal("Alexandra")
+    results.get(1).getString("age") should equal("24")
+
+    cluster.close()
+  }
+
 }
