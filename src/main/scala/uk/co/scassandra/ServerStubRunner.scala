d6db46f612f4da4344ca5e361b6d0a0aa0bc45c1
Christopher Batey <christopher.batey@gmail.com>
2014-03-16 17:58:44 -0600

    issue-16: re-introducing main method for server stub runner

--- src/main/scala/uk/co/scassandra/ServerStubRunner.scala
+++ src/main/scala/uk/co/scassandra/ServerStubRunner.scala
@@ -2,30 +2,30 @@ package uk.co.scassandra
 
 import akka.actor.{ActorSystem, Props}
 import com.typesafe.scalalogging.slf4j.Logging
-import priming.{PrimedResults, PrimingServer}
 import uk.co.scassandra.priming.{PrimingServer, PrimedResults}
 import uk.co.scassandra.server.TcpServer
 
-class ServerStubRunner extends Logging {
-
-  var portNumber = 8042
-  var primingPortNumber = 8043
-  var system : ActorSystem = _
-
-  val primedResults = PrimedResults()
-
+object ServerStubRunner extends Logging {
   def main(args: Array[String]) {
+    var portNumber = 8042
     if (args.length > 0) {
       val port = args(0)
       logger.info(s"Overriding port to $port")
       portNumber = port.toInt
     }
-    start()
+    new ServerStubRunner(portNumber).start()
   }
+}
+
+class ServerStubRunner(val serverPortNumber: Int = 8042, val primingPortNumber : Int = 8043) extends Logging {
+
+  var system : ActorSystem = _
+
+  val primedResults = PrimedResults()
 
   def start() = {
     system = ActorSystem("CassandraServerStub")
-    system.actorOf(Props(classOf[TcpServer], portNumber, primedResults))
+    system.actorOf(Props(classOf[TcpServer], serverPortNumber, primedResults))
     system.actorOf(Props(classOf[PrimingServer], primingPortNumber, primedResults))
     system.awaitTermination()
   }
