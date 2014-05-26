73176dde5a7c5089d867db91034a26f5bc2c11b4
Christopher Batey <christopher.batey@gmail.com>
2014-05-26 15:13:44 +0100

    removing un-used method.

--- src/main/scala/org/scassandra/priming/PrimeValidator.scala
+++ src/main/scala/org/scassandra/priming/PrimeValidator.scala
@@ -86,12 +86,6 @@ class PrimeValidator {
   private def convertValue(value: Any, columnType: ColumnType[_]): Any = {
     columnType.writeValue(value)
   }
-
-  private def hex2Bytes(hex: String): Array[Byte] = {
-    (for {i <- 0 to hex.length - 1 by 2 if i > 0 || !hex.startsWith("0x")}
-    yield hex.substring(i, i + 2))
-      .map(hexValue => Integer.parseInt(hexValue, 16).toByte).toArray
-  }
 }
 
 abstract class PrimeAddResult
